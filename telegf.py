605d09cb09248838188cb572f3173e80859d483e
thecsw <sagindyk.urazayev@protonmail.ch>
2017-11-16 13:50:07 +0400

    Added typing option and making the delay more realistic

--- telegf.py
+++ telegf.py
@@ -199,16 +199,20 @@ def sosf():
 # Send message from bot
 log_message('Initializing method for sending messages via telegram...', True)
 def sms(ID, str):
+    bot.sendChatAction(ID, 'typing')
     bot.sendMessage(ID, str)
 
 # Reply to messages
 log_message('Initializing method for replying to users\' messages...', True)
 def reply(ID, msgID, str):
+    bot.sendChatAction(ID, 'typing')
+    time.sleep(len(str)/5)
     bot.sendMessage(ID, str, None, None, None, msgID)
 
 # Send meessage with delay
 log_message('Initializing method for sending messages via telegram with delay...', True)
 def sms_delay(sec, ID, strg):
+    bot.sendChatAction(ID, 'typing')
     sleep(DELAY)
     sms(ID, strg)
 
